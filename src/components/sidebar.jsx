096fb4910c0ad70a810e66358d92189da6097ace
dcjmp90 <dcjmp90@gmail.com>
2020-09-29 18:07:21 -0500

    added padding for icons and added overflow code

--- src/components/sidebar.jsx
+++ src/components/sidebar.jsx
@@ -6,14 +6,14 @@ export default class Sidebar extends Component {
             <div>
                 <div>
                     <a href="#" className="js-colorlib-nav-toggle colorlib-nav-toggle" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar" style={{color: 'white'}}><i /></a>
-                    <aside id="colorlib-aside" role="complementary" className="border js-fullheight side-nav">
+                    <aside id="colorlib-aside" role="complementary" className="border js-fullheight" style={{overflow: 'hidden'}}>
                         <div className="text-center">
                             <div className="author-img" style={{ backgroundImage: 'url(/images/about.jpg)' }} />
                             <h1 id="colorlib-logo"><a href="index.html">Jonathan Perry</a></h1>
                             <span className="email"><i className="icon-mail"></i> jonathanmperry@gmail.com</span>
                             <span className="number"><i className="icon-phone"></i> +1 775 283 8248</span>
                         </div>
-                        <nav id="colorlib-main-menu" role="navigation" className="navbar">
+                        <nav id="colorlib-main-menu" role="navigation" className="navbar" style={{overflow: 'hidden'}}>
                             <div id="navbar" className="collapse">
                                 <ul>
                                     <li className="active"><a href="#" data-nav-section="home">Introduction</a></li>
@@ -24,8 +24,8 @@ export default class Sidebar extends Component {
                                 </ul>
                             </div>
                         </nav>
-                        <nav id="colorlib-main-menu" style={{textAlign: "center"}}>
-                            <div className="icon-row">
+                        <nav id="colorlib-main-menu" style={{textAlign: "center"}} style={{overflow: 'hidden'}}>
+                            <div className="icon-row" style={{paddingTop: '10px'}}>
                                 <div className="div-icon">
                                     <a href="#" target="_blank" className="icon-links" ><i className="icon-facebook2" /></a>
                                 </div>
