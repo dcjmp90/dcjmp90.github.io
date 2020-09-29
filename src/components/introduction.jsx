ac97d40b91aedcc2da3aac9945a199f81bacca5f
dcjmp90 <dcjmp90@gmail.com>
2020-09-29 17:20:18 -0500

    fix rgba () issues and put background color on the right div style

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -9,12 +9,12 @@ export default class Introduction extends Component {
             <ul className="slides">
               <li style={{backgroundImage: 'url(/images/img_bg2.jpg)' }}>
                 <div className="overlay" />
-                <div className="container-fluid" style={{color: '(rgba(0,0,0,.4)'}}>
+                <div className="container-fluid" style={{backgroundColor: 'rgba(0,0,0,.4)'}}>
                   <div className="row">
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
-                          <h1 style={{backgroundColor: 'white', fontSize: '50px'}}>HELLO! <br />I'M JON PERRY</h1>
+                          <h1 style={{color: 'white', fontSize: '50px'}}>HELLO! <br />I'M JON PERRY</h1>
                         </div>
                       </div>
                     </div>
