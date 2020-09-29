2eb82d55acee87a9ec57b60447c42bfd13bb5388
dcjmp90 <dcjmp90@gmail.com>
2020-09-29 17:28:23 -0500

    added inline flex option for text on slide 2

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -28,7 +28,7 @@ export default class Introduction extends Component {
                     <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
-                          <h1 style={{color: '#f15a22', fontSize: '60px'}}>VAIL <p style={{color: 'white', fontSize: '60px'}}>MEMBER</p></h1>
+                          <h1 style={{color: '#f15a22', fontSize: '60px'}}>VAIL <p style={{color: 'white', fontSize: '60px', display: 'inline-flex'}}>MEMBER</p></h1>
                           <h1 style={{color: 'white', fontSize: '30px'}}>COMPUTER VISION RESEARCHER</h1>
                         </div>
                       </div>
