fbfe066f59aeaab99141ac7b10ee74c7db572c1e
dcjmp90 <dcjmp90@gmail.com>
2020-09-30 17:21:53 -0500

    added new slide

--- src/components/introduction.jsx
+++ src/components/introduction.jsx
@@ -11,7 +11,7 @@ export default class Introduction extends Component {
                 <div className="overlay" />
                 <div className="container-fluid" style={{backgroundColor: 'rgba(0,0,0,.5)'}}>
                   <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                    <div className="col-md-12 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner js-fullheight">
                         <div className="desc">
                           <h1 style={{color: 'white', fontSize: '50px'}}>HELLO! <br />I'M JON PERRY</h1>
@@ -25,7 +25,7 @@ export default class Introduction extends Component {
                 <div className="overlay" />
                 <div className="container-fluid" style={{backgroundColor: 'rgba(0,0,0,.5)'}}>
                   <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                    <div className="col-md-12 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc">
                           <h1 style={{color: '#f15a22', fontSize: '60px'}}>VAIL <p style={{color: 'white', fontSize: '60px', display: 'inline-flex'}}>MEMBER</p></h1>
@@ -40,7 +40,7 @@ export default class Introduction extends Component {
                 <div className="overlay" />
                 <div className="container-fluid" style={{backgroundColor: 'rgba(0,0,0,.5)'}}>
                   <div className="row">
-                    <div className="col-md-6 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                    <div className="col-md-12 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
                       <div className="slider-text-inner">
                         <div className="desc" style={{textAlign: 'left', alignContent: 'left', position: 'relative'}}>
                           <div style={{width: '40%', height: 'auto'}}>
@@ -48,6 +48,23 @@ export default class Introduction extends Component {
                           </div>
                           
                           <h1 style={{color: 'white', fontSize: '40px'}}>GRADUATE STUDENT</h1>
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </li>
+              <li style={{background: 'url(/images/conference.jpg)', backgroundSize: 'cover' }}>
+                <div className="overlay" />
+                <div className="container-fluid" style={{backgroundColor: 'rgba(0,0,0,.5)'}}>
+                  <div className="row">
+                    <div className="col-md-12 col-md-offset-3 col-md-pull-3 col-sm-12 col-xs-12 js-fullheight slider-text">
+                      <div className="slider-text-inner">
+                        <div className="desc" style={{textAlign: 'left', alignContent: 'left', position: 'relative'}}>
+                          <div className="desc">
+                            <h1 style={{color: 'white', fontSize: '40px'}}>CHECK OUT <p style={{display: 'inline-flex', color:'#f15a22'}}>VAIL </p> RESEARCH</h1>
+                            <p className="border-links"><a className="btn btn-primary btn-learn border-links" href="https://cs.utsa.edu/research/facilities/#bottom-content" target="_blank">View Lab research <i className="fa fa-graduation-cap fa-2x" style={{paddingtop: '10px'}}/></a></p>
+                          </div>
                         </div>
                       </div>
                     </div>
