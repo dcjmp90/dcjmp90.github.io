16c114325de3f99a8dc131dd0352aa9c080418d0
dcjmp90 <dcjmp90@gmail.com>
2020-09-30 22:30:57 -0500

    added a message for mobile users

--- src/components/sidebar-mobile.jsx
+++ src/components/sidebar-mobile.jsx
@@ -40,6 +40,11 @@ export default class Sidebar extends Component {
                                 </div>
                             </div>
                         </nav>
+                        <div className="text-center">
+                            <span><p>--Mobile Version--</p></span>
+                            <span><p style={{fontSize:'6'}}>Use a Laptop/Desktop for a better experience!</p></span>
+                            <span><p style={{fontSize:'6'}}>Mobile site still in the works!</p></span>
+                        </div>
                     </aside>
                 </div>
             </div>
