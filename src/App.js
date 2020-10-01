650b7df285d0c77df756d9126c1eab9e70e3b736
dcjmp90 <dcjmp90@gmail.com>
2020-09-30 21:42:46 -0500

    added mobile support

--- src/App.js
+++ src/App.js
@@ -1,24 +1,36 @@
 import React, { Component } from 'react';
+import {
+  BrowserView,
+  MobileView,
+  isBrowser,
+  isMobile
+} from "react-device-detect";
 import Sidebar from './components/sidebar';
 import Introduction from './components/introduction';
 import About from './components/about';
 import Timeline from './components/timeline';
 
+
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
-      <div id="colorlib-page" style={{overflow: 'hidden'}}>
-        <div id="container-wrap">
-          <Sidebar></Sidebar>
-          <div id="colorlib-main" style={{overflow: 'hidden'}}>
-            <Introduction></Introduction>
-            <Timeline></Timeline>
+    if (isBrowser){
+      return (
+        <div id="colorlib-page" style={{overflow: 'hidden'}}>
+          <div id="container-wrap">
+            <Sidebar></Sidebar>
+            <div id="colorlib-main" style={{overflow: 'hidden'}}>
+              <Introduction></Introduction>
+              <Timeline></Timeline>
+            </div>
           </div>
         </div>
-      </div>
-    )
+      )
+    }
+    if (isMobile){
+      <div>Mobile App Coming soon!</div>
+    }
   }
 }
 
