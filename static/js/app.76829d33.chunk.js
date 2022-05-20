c1c3d142ba791b7b5895129f7f81d0824219cffa
Jon Perry <dcjmp90@gmail.com>
2022-05-20 18:42:13 -0500

    Updates

--- static/js/app.76829d33.chunk.js
+++
@@ -1,2 +0,0 @@
-(this.webpackJsonp=this.webpackJsonp||[]).push([[0],{65:function(t,e,n){t.exports=n(89)},89:function(t,e,n){"use strict";n.r(e);var r=n(93),c=n(64),s=(n(0),n(5)),a=n(58),o=n(21),i=n(20);var p=s.a.create({container:{flex:1,backgroundColor:"#fff",alignItems:"center",justifyContent:"center"}});Object(r.a)((function(){return Object(i.jsxs)(o.a,{style:p.container,children:[Object(i.jsx)(a.a,{children:"Open up App.js to start working on your app!"}),Object(i.jsx)(c.a,{style:"auto"})]})}))}},[[65,1,2]]]);
-//# sourceMappingURL=app.76829d33.chunk.js.map
